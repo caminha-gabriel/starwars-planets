a395c8ba973b80b2b200d1fa26e8178849c46a78
Caminha <gabrielcaminha255@gmail.com>
2022-06-08 16:48:38 -0300

    Requirement 01 completed; table component added; planets provider added on app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetsProvider>
+      <Table />
+    </PlanetsProvider>
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+function Table() {
+  const { data } = useContext(PlanetsContext);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Climate</th>
+          <th>Population</th>
+          <th>Gravity</th>
+          <th>Diameter</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>Url</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.population}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
