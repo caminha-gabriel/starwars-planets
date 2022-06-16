7d7b62b856164dd801c159244c99ba614e103940
Caminha <gabrielcaminha255@gmail.com>
2022-06-16 19:47:29 -0300

    changed numeric filter logics; added remove and add filter logics

--- src/components/FilterByNumericValue.js
+++ src/components/FilterByNumericValue.js
@@ -2,6 +2,14 @@ import React, { useState, useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function FilterByNumericValue() {
+  const [filterOptions, setFilterOptions] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+  const [filtersSelected, setFiltersSelected] = useState([]);
   const [selected, setSelected] = useState({
     column: 'population',
     comparison: 'maior que',
@@ -10,8 +18,31 @@ function FilterByNumericValue() {
 
   const { filterByNumericValues, setFilterByNumericValues } = useContext(PlanetsContext);
 
+  const removeFilterSelection = (filter) => {
+    const newArray = filtersSelected.filter(
+      (selectedFilter) => selectedFilter !== filter,
+    );
+    setFiltersSelected(newArray);
+    setFilterOptions([...filterOptions, filter]);
+  };
+
+  const removeFilters = () => {
+    setFilterOptions([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+    setFiltersSelected([]);
+  };
+
   const numericFilterSubmit = (e) => {
     e.preventDefault();
+
+    const newFilterOptions = filterOptions.filter((option) => option !== selected.column);
+    setFilterOptions(newFilterOptions);
+    setFiltersSelected([...filtersSelected, selected.column]);
 
     setFilterByNumericValues([...filterByNumericValues, selected]);
     setSelected({
@@ -27,21 +58,20 @@ function FilterByNumericValue() {
         data-testid="column-filter"
         value={ selected.column }
         onChange={ ({ target }) => setSelected({
-          ...selected, column: target.value,
+          ...selected,
+          column: target.value,
         }) }
       >
-        <option>population</option>
-        <option>orbital_period</option>
-        <option>diameter</option>
-        <option>rotation_period</option>
-        <option>surface_water</option>
+        {filterOptions.map((column) => (
+          <option key={ column }>{column}</option>
+        ))}
       </select>
-
       <select
         data-testid="comparison-filter"
         value={ selected.comparison }
         onChange={ ({ target }) => setSelected({
-          ...selected, comparison: target.value,
+          ...selected,
+          comparison: target.value,
         }) }
       >
         <option>maior que</option>
@@ -58,9 +88,29 @@ function FilterByNumericValue() {
           value: Number(target.value),
         }) }
       />
+      <div>
+        {filtersSelected.map((filter) => (
+          <div key={ filter }>
+            <span>{`The ${filter} filter is selected`}</span>
+            <button
+              type="button"
+              onClick={ () => removeFilterSelection(filter) }
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
 
       <button data-testid="button-filter" type="submit">
         Filtrar
+      </button>
+      <button
+        data-testid="button-remove-filters"
+        type="button"
+        onClick={ removeFilters }
+      >
+        Remover Filtros
       </button>
     </form>
   );
