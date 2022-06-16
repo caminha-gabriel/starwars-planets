0215b9bd6a6ab4a6f061214677cfbb90298d1be2
Caminha <gabrielcaminha255@gmail.com>
2022-06-16 06:30:36 -0300

    component who filters data by name input added

---
+++ src/components/FilterByName.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+function FilterByName() {
+  const { filterByName, setFilterByName } = useContext(PlanetsContext);
+  return (
+    <div>
+      <label htmlFor="filterPlanetsByName">
+        <input
+          data-testid="name-filter"
+          value={ filterByName }
+          onChange={ ({ target }) => setFilterByName(target.value) }
+          id="filterPlanetsByName"
+        />
+      </label>
+    </div>
+  );
+}
+
+export default FilterByName;
