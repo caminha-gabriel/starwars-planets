45d80c4bb49bda2a3686ba925ed135f86b42338f
Caminha <gabrielcaminha255@gmail.com>
2022-07-07 04:06:58 -0300

    added filter styles

--- src/components/PlanetFilter.js
+++ src/components/PlanetFilter.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import FilterByName from './FilterByName';
 import FilterByNumericValue from './FilterByNumericValue';
+import '../styles/PlanetFilter.css';
 
 function PlanetFilter() {
   return (
-    <div>
+    <div className="planet-filter-div header-info">
       <FilterByName />
       <FilterByNumericValue />
     </div>
